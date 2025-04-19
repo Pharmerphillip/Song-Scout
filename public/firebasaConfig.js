a8ed2205a382924903fc9f819b4de78d2040a8bb
Pharmerphillip <tristan0mungal@gmail.com>
2025-04-18 21:40:32 -0400

    Changes v4

---
+++ public/firebasaConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB0ZxXlq8KHN1jWOk7Iq5XXyPHGxmsk594",
+    authDomain: "songscout-3c222.firebaseapp.com",
+    projectId: "songscout-3c222",
+    storageBucket: "songscout-3c222.firebasestorage.app",
+    messagingSenderId: "318654746581",
+    appId: "1:318654746581:web:a54b51cd8b3bdff131775c",
+    measurementId: "G-CF8QFV8RNZ"
+  };
+
+  export default firebaseConfig
